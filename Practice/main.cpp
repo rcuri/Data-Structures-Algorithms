46b8dffbed9c55b1874a4e2402cbfafb8777fab3
Rodrigo Curiel <rodrigocuriel@Rodrigos-MacBook-Air.local>
2017-10-22 00:08:37 -0700

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Rodrigo Curiel on 10/22/17.
+//  Copyright © 2017 Rodrigo Curiel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
